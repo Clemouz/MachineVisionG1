2cdba722f669e38eb01e4b44690cfa0437a122da
Jonathan <jonathancasemtr@gmail.com>
2025-04-21 13:50:04 +0200

    Added calculate RMS height function and visualizing for createGrid.py

--- createGrid.py
+++ createGrid.py
@@ -5,7 +5,7 @@ from rasterio.transform import from_bounds
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
-las_file = "Points.laz"
+las_file = "240829_ALS_Matrice300_Svb_Classified.las"
 
 # Read LAS file and get bounds
 def read_laz_bounds(filename):
@@ -28,6 +28,20 @@ def interpolate_points(x, y, z, grid_x, grid_y):
     points = np.column_stack((x, y))
     interpolated_z = griddata(points, z, grid_points, method='nearest')
     return interpolated_z.reshape(grid_x.shape)
+
+
+def calculate_rms(dtm_array, window_size):
+    rows, cols = dtm_array.shape
+    rms_map = np.full_like(dtm_array, np.nan, dtype=np.float32)
+
+    for i in range(0, rows, window_size):
+        for j in range(0, cols, window_size):
+            window = dtm_array[i:i+window_size, j:j+window_size]
+           
+            rms = np.std(window)
+            rms_map[i:i+window_size, j:j+window_size] = rms
+
+    return rms_map
 
 # Save the generated raster as "GeoTIFF"
 def save_raster(filename, x, y, data):
@@ -55,4 +69,13 @@ grid_x, grid_y = create_grid(min_x, min_y, max_x, max_y, resolution=0.1)
 ground_points = (classification == 2)
 dtm_z = interpolate_points(x[ground_points], y[ground_points], z[ground_points], grid_x, grid_y)
 save_raster("dtm.tif", grid_x, grid_y, dtm_z)
-show_raster("dtm.tif", "Digital Terrain Model")
+#show_raster("dtm.tif", "Digital Terrain Model")
+
+with rasterio.open("dtm.tif") as src:
+    dtm = src.read(1)
+    transform = src.transform
+
+rms_map = calculate_rms(dtm, window_size=20)
+
+save_raster("rms_height_map.tif", grid_x, grid_y, rms_map)
+show_raster("rms_height_map.tif", "RMS Height per Patch")
